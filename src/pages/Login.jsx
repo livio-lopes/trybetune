6d5965c19b3df6e2e58dd4ef08dc4a5c097ae270
Lívio Lopes <antonio.lvio760@gmail.com>
2023-01-18 12:51:38 -0300

    requisito 2 em processo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,15 +8,46 @@ import ProfileEdit from './ProfileEdit';
 import NotFound from './NotFound';
 
 export default class Login extends Component {
+  state = {
+    loginNameInput: '',
+  };
+
+  handleChange = ({ target }) => {
+    {}
+    this.setState({
+      [name]=value,
+    })
+  };
+
   render() {
     return (
       <div data-testid="page-login">
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="*" component={ NotFound } />
+        <form>
+          <label htmlFor="loginNameInput">
+            <input
+              type="text"
+              name="loginNameInput"
+              id="loginNameInput"
+              placeholder="Nome de Usuário"
+              data-testid="login-name-input"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+          >
+            Entrar
+
+          </button>
+        </form>
+        <Switch>
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </div>
 
     );
