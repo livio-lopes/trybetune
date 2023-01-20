e114b29b18406a86ffd6adc12b0e0ed2e3f84a39
Lívio Lopes <antonio.lvio760@gmail.com>
2023-01-20 13:01:58 -0300

    Requisito 2 resolvido

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,68 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Search from './Search';
 import Album from './Album';
 import Favorites from './Favorites';
 import Profile from './Profile';
 import ProfileEdit from './ProfileEdit';
 import NotFound from './NotFound';
+import { createUser } from '../services/userAPI';
+import LoadingScreen from '../components/LoadingScreen';
 
 export default class Login extends Component {
+  state = {
+    inputName: '',
+    disableLogin: true,
+    loadindScreen: false,
+    logged: undefined,
+  };
+
+  handleLogin = ({ target }) => {
+    const { value } = target;
+    const minInput = 3;
+    this.setState({
+      inputName: value,
+      disableLogin: value.length < minInput,
+    });
+  };
+
+  clickLogin = () => {
+    const { inputName } = this.state;
+    this.setState({ loadindScreen: true });
+    createUser({ name: inputName })
+      .then((r) => this.setState({ logged: r,
+        loadindScreen: false }));
+  };
+
+  renderForm = () => {
+    const { inputName, disableLogin } = this.state;
+    return (
+      <form action="">
+        <label htmlFor="inputName">
+          <input
+            data-testid="login-name-input"
+            type="text"
+            name="inputName"
+            id="inputName"
+            value={ inputName }
+            onChange={ this.handleLogin }
+            placeholder="Qual o seu nome?"
+          />
+        </label>
+        <button
+          data-testid="login-submit-button"
+          type="button"
+          disabled={ disableLogin }
+          onClick={ () => { this.clickLogin(); } }
+        >
+          Entrar
+        </button>
+      </form>
+    );
+  };
+
   render() {
+    const { loadindScreen, logged } = this.state;
     return (
       <div data-testid="page-login">
         <Switch>
@@ -18,10 +72,10 @@ export default class Login extends Component {
           <Route exact path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
           <Route path="*" component={ NotFound } />
-
         </Switch>
+        {loadindScreen ? <LoadingScreen /> : this.renderForm()}
+        {logged && <Redirect to="/search" />}
       </div>
-
     );
   }
 }
