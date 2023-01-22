82780ac32c1d40f988e35a2b19457524915f8ab8
Lívio Lopes <antonio.lvio760@gmail.com>
2023-01-22 11:41:27 -0300

    Requisito 5 resolvido

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,12 +3,53 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    inputSearch: '',
+    disableSearch: true,
+  };
+
+  handleSearch = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      inputSearch: value,
+      disableSearch: value.length < 2,
+    });
+  };
+
+  renderSearch = () => {
+    const { inputSearch, disableSearch } = this.state;
+    return (
+      <form>
+        <label htmlFor="inputSearch">
+          <input
+            type="text"
+            name="inputSearch"
+            id="inputSearch"
+            value={ inputSearch }
+            onChange={ this.handleSearch }
+            data-testid="search-artist-input"
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ disableSearch }
+          data-testid="search-artist-button"
+        >
+          Pesquisar
+
+        </button>
+      </form>
+    );
+  };
+
   render() {
     const { userName } = this.props;
     return (
       <div data-testid="page-search">
         Search
         <Header userName={ userName } />
+        {this.renderSearch()}
+
       </div>
     );
   }
